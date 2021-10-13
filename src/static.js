ed9432d220e554616ca193bb1953c132e6c9b353
Jordi Yaputra <jordiyaputra@gmail.com>
2021-10-13 07:36:33 +0000

    chore: move BoxDebugger out

--- src/static.js
+++ src/static.js
@@ -11,69 +11,8 @@ const canvas = new fabric.Canvas("main-canvas", {
   backgroundColor: "#a0a0a0",
   selection: false,
   hoverCursor: "none",
+  renderOnAddRemove: false,
 });
-
-class BoxDebugger extends fabric.Group {
-  constructor() {
-    const bg = new fabric.Rect({
-      top: 150,
-      left: 150,
-      width: 200,
-      height: 150,
-      fill: "#000000",
-    });
-
-    const mid = new fabric.Rect({
-      top: bg.top - 40,
-      left: bg.left + bg.width / 2,
-      width: 40,
-      height: 40,
-      fill: "yellow",
-    });
-
-    const rect1 = new fabric.Rect({
-      width: 30,
-      height: 30,
-      fill: "green",
-    });
-    const rect2 = new fabric.Rect({
-      width: 20,
-      height: 20,
-      fill: "red",
-    });
-    const text1 = new fabric.Text("Lorem Ipsum!", {
-      fontSize: 12,
-      fill: "yellow",
-      stroke: 0,
-    });
-
-    const box2 = new Box([rect1, rect2, text1], {
-      backgroundColor: "blue",
-      // top: 150,
-      // left: 150,
-      padding: 10,
-      width: 200,
-      // height: 100,
-      direction: "row",
-      justify: "start",
-      align: "center",
-      autoLayout: true,
-      spacing: 5,
-    });
-
-    const box3 = fabric.util.object.clone(box2);
-    box3.top = 30;
-    console.log(box3 === box2);
-    const wrapper = new Box([box2, box3], {
-      autoLayout: true,
-      direction: "column",
-      top: 150,
-      left: 150,
-    });
-
-    super([bg, wrapper, mid]);
-  }
-}
 
 const loader = new ImageLoader();
 loader
@@ -91,15 +30,26 @@ loader
     const os = new OperatingSystemUI(assets, {});
     canvas.add(os);
 
-    canvas.on("mouse:move", (e) => {
-      os.moveCursor(e.pointer);
+    const onCursorMove = ({ x, y }) => {
+      os.moveCursor({ x, y });
       canvas.renderAll();
-    });
-    canvas.on("mouse:up", (e) => {
+    };
+
+    const onCursorClick = () => {
       os.click();
       canvas.renderAll();
+    };
+
+    canvas.on("mouse:move", (e) => {
+      onCursorMove(e.pointer);
     });
+    canvas.on("mouse:up", (e) => {
+      onCursorClick();
+    });
+
     // const _debugger = new BoxDebugger();
     // canvas.add(_debugger);
+
+    canvas.renderAll();
   })
   .catch(console.error);
